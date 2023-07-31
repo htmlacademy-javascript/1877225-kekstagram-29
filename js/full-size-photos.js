422c1050583c186b3d3f0a7dad35d49bec0fb143
Елизавета <elizaweta.petro2011@yandex.ru>
2023-07-31 19:42:03 +0300

    Добавляет функцию отображения комментариев по клику на кнопку

--- js/full-size-photos.js
+++ js/full-size-photos.js
@@ -11,6 +11,7 @@ const commentList = document.querySelector('.social__comments');
 const commentTemplate = document.querySelector('#comment').content;
 const likesCount = document.querySelector('.likes-count');
 const socialCaption = document.querySelector('.social__caption');
+const currentCommentCount = document.querySelector('.comments-count--current');
 
 const createComment = ({ avatar, message, name }) => {
   const commentElement = commentTemplate.cloneNode(true);
@@ -32,9 +33,33 @@ const showComments = (comments) => {
   commentList.append(picturesPreviewFragment);
 };
 
+let startCommentsCount = 5;
+
+const showCommentsParts = (comments) => {
+  if (comments.length > 5) {
+    commentsLoader.classList.remove('hidden');
+    socialCommentCount.classList.remove('hidden');
+    showComments(comments.slice(0, 5));
+    const COMMENTS_STEP = 5;
+    commentsLoader.addEventListener('click', () => {
+      console.log('вызываю обработчик');
+      const n = startCommentsCount + COMMENTS_STEP;
+      startCommentsCount = n;
+      showComments(comments.slice(0, n));
+      currentCommentCount.textContent = n;
+      if (n >= comments.length) {
+        commentsLoader.classList.add('hidden');
+        currentCommentCount.textContent = comments.length;
+      }
+    });
+  }
+};
+
 const closeModal = () => {
   bigPictureOverlay.classList.add('hidden');
   body.classList.remove('modal-open');
+  startCommentsCount = 5;
+  currentCommentCount.textContent = startCommentsCount;
 };
 
 const hidePictureOverlay = () => {
@@ -59,5 +84,6 @@ export const thumbnailClickHandler = (url, description, likes, comments) => {
   commentCount.textContent = comments.length;
   likesCount.textContent = likes;
   showComments(comments);
+  showCommentsParts(comments);
   hidePictureOverlay();
 };
